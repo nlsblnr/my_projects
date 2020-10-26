52e68c7471b2e03a342c87a2d81cb75b192b291b
Nils Blunier <73489655+nlsblnr@users.noreply.github.com>
2020-10-26 18:08:30 +0100

    Update fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 x = []
 y = []
 
-def fib(anzahl):
+def fib(quantity):
     l = [0, 1]
-    for x in range(anzahl - 2):
+    for x in range(quantity - 2):
         l.append(int(l[len(l) - 1]) + int(l[len(l) - 2]))
     
     y.append(l[len(l) - 1])
@@ -13,12 +13,9 @@ def fib(anzahl):
     print('List for ' + str(r) + ' Cases:')
     print(l)
 
-try:
-    for r in range(1, int(input('Enter case quantity:\n')) + 1):
-        fib(r)
-        x.append(r)
-except:
-    print('\nNOOOO\n')
+for r in range(1, int(input('Enter quantity of n:\n')) + 1):
+    fib(r)
+    x.append(r)
 
 fig = plt.figure()
 ax = fig.add_axes([0, 0, 1, 1])
