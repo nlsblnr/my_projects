eaf09577f2c3d2e9de8ef88ea08692c2e3c9297f
Nils Blunier <73489655+nlsblnr@users.noreply.github.com>
2020-12-06 17:06:47 +0100

    Add files via upload
    I uploaded a script in which I used the yield method for the first time ever. I actually made two different small exercises in the same script. If you looked at my code I would really appreciate it if you reviewed it and told me your honest opinion.

---
+++ yield_studying.py
@@ -0,0 +1,37 @@
+my_list = ["aa", "bb", "cc", "dd", "ee", "ff"]
+
+def some_algorithm():
+    for item in my_list:
+        yield(item)
+
+develop = some_algorithm()
+
+for x in range(len(my_list)):
+    print(next(develop))
+
+var_list = ["a", "b", "c", "d", "e", "f", "g"]
+val_list = [1, 2, 3, 4, 5, 6, 7]
+
+def generate_variables():
+    for index, obj in enumerate(var_list):
+        current_item = val_list[index]
+        globals()[obj] = current_item
+        yield(current_item)
+
+main = generate_variables()
+
+try:
+    for generate in range(len(var_list)):
+        next_iter = next(main)
+        if val_list[generate] == next_iter:
+            print(val_list[generate])
+        else:
+            print(val_list[generate])
+            print(next_iter)
+            print("\n")
+
+except:
+    print("UNEXPECTED ERROR OCCURRED")
+
+finally:
+    print("\nPROGAMM EXECUTED WITH RETURN 0")
